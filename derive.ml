b68ec312f69fb1d2b785aa9785fe8153baa1f1ce
Jesper Andersen <jespera@gmail.com>
2011-08-16 07:24:39 +0200

    New implementation of derivative CFG parser
    Differences from previous implementation
    
    - uses finite maps to represent rules as well as information about
      nullability of non-terminals
    - uses the lazy-construct on derived rules to defer computations of
      derived rules until actually needed (hopefully)

--- derive.ml
+++ derive.ml
@@ -1,301 +1,315 @@
-(* It seems the idea is that taking a derivative of a grammar, we
- * should rewrite the grammar and then we can defer the decision of
- * whether a string matched a grammar until having consumed the 
- * entire string. I.e. 
- * rec(ct, G) = rec(t, Dc(G))
- * rec([], G) = nullable(G)
- *)
+let new_fix no_new comb f = function y ->
+  let rec loop x =
+    let fx = f x in
+    if no_new fx x then x else loop (comb fx x)
+  in
+    loop y
 
-type pattern = Epsilon | Empty | Lit of char | NT of string
-type rule    = Rule of string * (pattern list)
-type grm     = Grm of string * (rule list)
+let fix eq f x = 
+  let rec loop x =
+    let fx = f x in
+    if eq fx x then x else loop fx 
+  in
+    loop x
 
-let chr c = [Lit c]
+let opt_map_tail f xs =
+  let rec loop acc = function 
+    | [] -> acc
+    | x :: xs -> 
+        match f x with
+        | None -> loop acc xs
+        | Some v -> loop (v :: acc) xs
+  in
+  loop [] xs
 
-let explode s =
-	let rec loop n = 
-		if n < String.length s
-		then (s.[n]) :: loop (n + 1)
-		else []
-	in
-		loop 0
+
+let (++) x xs = if List.mem x xs then xs else x :: xs
+
+let (@@) xs ys = List.fold_left (fun zs x -> x ++ zs) ys xs
+
+module Grammar =
+  struct
+    type pattern   = Epsilon | Empty | Lit of char | NT of string
+    type pre_rule  = Rule of pattern list list | Derived of pre_rule Lazy.t 
+
+    module RuleOrder = struct
+      type t = string
+      let compare = Pervasives.compare
+    end
+    module RuleMap = Map.Make(RuleOrder)
+    type rules = pre_rule RuleMap.t    
+        
+    module NullOrder = struct
+      type t = string
+      let compare = Pervasives.compare
+    end
+    module NullMap = Map.Make(NullOrder)
+    type nullables = bool option NullMap.t
+    (* should only allow monotonic additions where None < Some false 
+     * and None < Some true and nothing else *)
+
+    let string_of_null_opt = function
+      | None -> "⏊"
+      | Some f -> string_of_bool f
+
+    let string_of_nullmap nm =
+      NullMap.fold (fun name value acc -> name ^ "->" ^ string_of_null_opt value ^ " " ^ acc) nm ""
 
 
-let str s = 
-	let rec loop n = 
-		if n < String.length s
-		then chr (s.[n]) @ loop (n + 1)
-		else []
-	in
-		loop 0
+    type grm       = {
+      start:string; 
+      rules:rules;
+    }
 
-exception No_rule
-(* construct a rule *)
-let (+>) name ps = [Rule (name, ps)]
+    let (!!) r = match r with
+    | Rule _ -> r
+    | Derived cont -> Lazy.force cont
+ 
+    let rec string_of_pre = function
+      | Rule(ps_lists) -> "["^String.concat " | " 
+        (
+          List.map (function ps_list -> 
+            "[" ^ String.concat " " (List.map (function
+                | Epsilon -> "ε" 
+                | Empty   -> "Ø"
+                | Lit c   -> String.make 1 c
+                | NT s    -> s) ps_list)
+            ^ "]"
+          ) ps_lists
+        )
+      ^"]"
+      | Derived v when Lazy.lazy_is_val v -> 
+          "?<" ^ string_of_pre (Lazy.force v) ^ ">"
+      | Derived v -> "?"
 
-(* add branch to rule *)
-let (|>) (rules : rule list) ps =
-	match rules with 
-	| [] -> raise No_rule
-	| (Rule (name, _)) :: rs -> Rule (name, ps) :: rules
+    let string_of_rule name pre_rule acc =
+      (name ^ " ::= " ^ string_of_pre pre_rule) :: acc
 
-let (!!) name = [NT name]
+    let string_of_rules rules =
+      String.concat "\n" (RuleMap.fold string_of_rule rules [])
 
-let digit =
-	"digit" +> 
-		chr '0' |> chr '1' |> chr '2' |> chr '3' |>
-		chr '4' |> chr '5' |> chr '6' |> chr '7' |>
-		chr '8' |> chr '9'
+    let make_grm start = 
+      {
+        start = start;
+        rules = RuleMap.empty;
+      }
 
-let num =
-	"num" +> !!"digit" |> !!"digit" @ !!"num"
+    exception Error
+    exception Lazy_error
 
-let ope =
-  "ope" +> 
-    chr '+' |> chr '-' |> chr '*' |> chr '/'
+    (* Return the rules that can be reached from the start-node *)
+    let get_reachable grm = 
+      let start_ps_lists = !!(RuleMap.find grm.start grm.rules) in
+      let start_rules =
+        RuleMap.add grm.start start_ps_lists RuleMap.empty in
+      (* given the rules in cur_rules, find all rules from grm.rules
+       * that are referenced from cur_rules
+       *)
+      let f cur_rules_map = 
+        (* for each name->pre_rule binding in cur_rules_map
+         * we scan the ps_lists and for each (NT name') we 
+         * add the pair (name', cur_rules.map[name']) to the output set
+         * (if not already added)
+         *)
+        RuleMap.fold 
+        (fun name rule acc_new -> 
+          match rule with
+          | Rule ps_lists ->
+              List.fold_left (fun acc_new ps_list -> 
+                List.fold_left (fun acc_new p -> 
+                  match p with 
+                  | NT name' -> 
+                      if RuleMap.mem name' cur_rules_map
+                      then acc_new
+                      else begin
+                        try
+                          (name', RuleMap.find name' grm.rules) :: acc_new 
+                        with Not_found -> 
+                          (* partial grammar *)
+                          acc_new
+                      end
+                  | _ -> acc_new
+                ) acc_new ps_list
+              ) 
+              acc_new ps_lists
+          | Derived _ -> raise Lazy_error
+        )
+        cur_rules_map
+        []
+      in
+      new_fix 
+      (fun new_rules old_rules_map -> 
+        begin
+          (*print_endline ("New rules: " ^*)
+            (*String.concat " " (List.map (fun (n,_) -> n) new_rules)*)
+          (*);*)
+          new_rules = []
+        end)
+      (fun new_rules cur_rules_map -> 
+        (* add new_rules to cur_rules_map *)
+        List.fold_left (fun acc_map (name, pre_rule) -> 
+          RuleMap.add name !!pre_rule acc_map
+        ) cur_rules_map new_rules
+      )
+      f start_rules
 
-let expr =
-  "expr" +> (!!"expr" @ !!"ope" @ !!"expr")
-         |> (chr '(' @ !!"expr" @ chr ')')
-         |> !!"num"
+    let contains_true = function
+      | None -> false
+      | Some false -> false
+      | Some true -> true
 
-let expr_grm =
-	Grm ("expr", expr @ ope @ num @ digit)
+    let compute_nullables grm = 
+      let reachable_rules = get_reachable grm in
+      let eq fx x = fx = x in
+      let rec compute_null name ps_lists is_null acc_nulls =
+        let p_null = function
+          | Epsilon -> true
+          | Empty 
+          | Lit _ -> false
+          | NT s -> 
+              if NullMap.mem s acc_nulls 
+              then contains_true (NullMap.find s acc_nulls)
+              else false
+        in
+        match ps_lists with 
+        | [] -> is_null
+        | ps_list :: ps_lists ->
+            if List.for_all p_null ps_list && ps_list != []
+            then Some true
+            else compute_null name ps_lists is_null acc_nulls
+      in
+      let g name pre_rule acc_nulls =
+        match !!pre_rule with
+        | Rule ps_lists -> 
+            let is_nullable = compute_null name ps_lists None acc_nulls in
+            NullMap.add name is_nullable acc_nulls
+        | _ -> raise Error
+      in
+      let f x =
+        (* pass each rule and compute nullability status of all symbols;
+         * if a symbol has a rule S -> [[ε];...] then its nullable or if
+         * it references a rule already known to be nullable
+         *)
+        RuleMap.fold g reachable_rules x in
+      (*let () = print_endline "starting fix-point iteration" in*)
+      fix eq f NullMap.empty
 
-let num_grm =
-  Grm ("num", num @ digit)
+    let derive_x_nt x name = "d" ^ String.make 1 x ^ "["^name^"]"
 
-let s = 
-  "S" +> (!!"S" @ chr '+' @ !!"S")
-      |> chr '1'
+
+    (* A call 'derive x grm' constructs (lazily) a new grammar that corresponds
+     * to grm after x has be accepted. *)
+    let derive_grm x grm = 
+      let derive_rule rule_name pre_rule = 
+        (*print_endline ("derive_rule called for " ^ rule_name);*)
+        let null_map = compute_nullables grm in
+        let rec f acc = function 
+          | [] -> acc
+          | Epsilon :: ps -> f acc ps
+          | Empty :: _ -> acc
+          | Lit c :: ps -> 
+              if c = x
+              then 
+                if ps = [] then [Epsilon] :: acc
+                else ps :: acc
+              else acc
+          | NT s :: ps ->
+              match NullMap.find s null_map with
+              | Some false
+              | None -> (NT (derive_x_nt x s) :: ps) :: acc
+              | Some true -> 
+                  f ((NT (derive_x_nt x s) :: ps) :: acc) ps
+        in 
+        match pre_rule with
+        | Rule ps_lists -> 
+            Rule (List.fold_left f [] ps_lists)
+        | _ -> raise Lazy_error
+      in
+      let lazy_derive_rule rule_name pre_rule acc_rules = 
+        begin
+          (*print_endline ("lazy_derive for " ^ rule_name);*)
+          RuleMap.add 
+          (derive_x_nt x rule_name) 
+          (Derived (lazy (derive_rule rule_name pre_rule)))
+          acc_rules
+        end
+      in
+      let reachable_rules = get_reachable grm in
+      (* for each reachable rule, construct the derived rule and add it to the
+       * grammar given *)
+      {start = derive_x_nt x grm.start;
+       rules = RuleMap.fold lazy_derive_rule reachable_rules grm.rules 
+      }
+
+
+
+    let add_rule name p_rule grm =
+      {grm with 
+        rules = RuleMap.add name p_rule grm.rules;
+      }
+
+    let explode str =
+      let s_len = String.length str in
+      let rec loop n =
+        if n = s_len then []
+        else str.[n] :: loop (n + 1)
+      in
+        loop 0
+
+    let recognize str grm = 
+      begin
+        print_endline ("Trying to recognize string: " ^ str);
+        let rec loop grm = function
+          | [] -> 
+              begin
+                let res = NullMap.find grm.start (compute_nullables grm) in
+                print_endline "Done deriving; resulting grammar:";
+                print_endline (string_of_rules grm.rules);
+                print_endline ("start: " ^ grm.start);
+                res
+              end
+          | c :: cs -> loop (derive_grm c grm) cs
+        in
+          loop grm (explode str)
+      end
+  end
 
 let s_grm =
-  Grm ("S", s)
+  let open Grammar in
+  add_rule "Q" (Rule [[Epsilon]]) (
+  add_rule "S" (Rule [
+    [NT "Q"]; 
+    [NT "S"; Lit '('; NT "S"; Lit ')']
+    ]) (make_grm "S")
+  )
 
-let pp_pattern = function
-	| Epsilon -> "ε"
-	| Empty   -> "Ø"
-	| Lit c   -> String.make 1 c
-	| NT n    -> n
-let pp_rule = function
-	| Rule (s, patterns) -> s ^ "->" ^ String.concat " " (List.map pp_pattern patterns)
-let pp_grm = function
-	| Grm (start, rules) -> 
-			"start: " ^ start ^ "\n"  ^ 
-			String.concat "\n" (List.map pp_rule rules) ^
-			"\n"
-			
-let list_grm = Grm("List", [
-	Rule ("List", [Lit 'x']);
-	Rule ("List", [NT "List"; Lit 'x']);
-])
-
-let paren_grm = Grm("S",[
-	Rule ("S", [Epsilon]);
-	Rule ("S", [NT "S"; Lit '('; NT "S"; Lit ')']);
-])
-
-(* Return the list of rules defining a NT symbol *)
-let get_rules grm name =
-	match grm with 
-	| Grm(_, rules) -> List.find_all (fun (Rule (name', ps)) -> name' = name) rules
-
-let make_nt x s = "d" ^ (String.make 1 x) ^ "(" ^ s ^ ")"
-
-let derive_pattern x = function 
-	| Lit y when y = x -> Epsilon
-	| Epsilon
-	| Empty 
-	| Lit _ -> Empty
-	| NT s -> NT (make_nt x s)
-
-let rec fix eq f x =
-	let fx = f x in
-	if eq fx x then fx else fix eq f fx
+let list_grm =
+  let open Grammar in
+  add_rule "List" (Rule[
+    [Lit 'x'];
+    [NT "List"; Lit ';'; Lit 'x']
+  ]) (make_grm "List")
 
 
-let eq_lists l1 l2 = 
-	List.for_all (fun e1 -> List.exists (fun e2 -> e1 = e2) l2) l1 &&
-	List.for_all (fun e2 -> List.exists (fun e1 -> e1 = e2) l1) l2
-	
-let (++) e ls = if List.exists (fun e' -> e = e') ls then ls else e :: ls
-let (@@) ls1 ls2 = List.fold_left (fun acc_ls e1 -> e1 ++ acc_ls) ls2 ls1
-
-let nullable_cache =
-  Hashtbl.create 101
-
-let cached_find s =
-  if Hashtbl.mem nullable_cache s
-  then Some (Hashtbl.find nullable_cache s)
-  else None
-
-let all_symbs = Hashtbl.create 101
-
-let rec refresh_all_symbs = function
-  | [] -> ()
-  | (Rule(s,_)) :: rs -> 
-      begin 
-        Hashtbl.replace all_symbs s true;
-        refresh_all_symbs rs;
-      end
-
-let add_nullable s = (Hashtbl.replace nullable_cache s true; s)
-let is_nullable s = Hashtbl.find nullable_cache s
-
-(* returns the set of nullable symbols in a grammar *)
-let nullable_symbs = function
-	| Grm(start, rules) -> 
-		let rec g acc_symbs = function 
-			| Rule(s, []           )-> []
-			| Rule(s, Lit _   :: _ )
-			| Rule(s, Empty   :: _ )-> []
-			| Rule(s, Epsilon :: [])-> [add_nullable s]
-			| Rule(s, Epsilon :: ps)-> g acc_symbs (Rule (s, ps))
-			| Rule(s, NT s'   :: ps)-> 
-          match cached_find s' with
-          | None ->
-                if List.exists (fun a -> a = s') acc_symbs
-                then 
-                  if ps = []
-                  then [add_nullable s]
-                  else g acc_symbs (Rule(s, ps))
-                else []	
-          | Some true ->
-              if ps = []
-              then [add_nullable s]
-              else g acc_symbs (Rule(s,ps))
-          | Some false -> []
-		in 
-    let null_symbs = 
-      fix eq_lists 
-				(fun old_symbs -> 
-					List.fold_left 
-					(fun acc ls -> ls @@ acc) 
-					[] 
-					(List.rev_map (g old_symbs) rules))
-        [] in
-    let () = refresh_all_symbs rules in
-    begin
-      Hashtbl.fold (fun symb _ _ ->
-        if Hashtbl.mem nullable_cache symb
-        then ()
-        else Hashtbl.replace nullable_cache symb false;
-      ) all_symbs ();
-      null_symbs;
-    end
-
-
-let nullable_p grm = function 
-	| Lit _ | Empty -> false
-	| Epsilon -> true
-	| NT s -> 
-      if Hashtbl.mem nullable_cache s
-      then Hashtbl.find nullable_cache s
-      else 
-        begin
-          let symb = nullable_symbs grm in
-          try is_nullable s
-          with Not_found -> begin
-            (*print_endline ("Symbol not in cache: " ^ s);*)
-            (*print_endline ("Cache   : " ^ *)
-              (*Hashtbl.fold (fun key _ acc -> key ^ " " ^ acc) nullable_cache "");*)
-            (*print_endline ("Computed: " ^ String.concat " " symb);*)
-            (*print_endline ("Gramar follows");*)
-            (*print_endline (pp_grm grm);*)
-            (*raise Not_found*)
-            Hashtbl.replace nullable_cache s false;
-            false;
-          end
-        end
-			
-
-let compact_rule = function
-  | Rule(s, ps) as rule ->
-      if List.exists (fun p -> p = Empty) ps
-      then None
-      else Some rule
-
-let derive_rule_cache = Hashtbl.create 101
-
-let rec derive_rule grm x = function
-	| Rule(s, ps) as rule-> 
-      if Hashtbl.mem derive_rule_cache (x, rule)
-      then begin
-        Hashtbl.find derive_rule_cache (x, rule)
-      end
-      else 
-        let res =
-          match ps with
-          | [] -> []
-          | p :: ps ->
-              match compact_rule (Rule (make_nt x s, derive_pattern x p :: ps)) with
-              | None -> 
-                  if nullable_p grm p
-                  then derive_rule grm x (Rule (s, ps))
-                  else []
-              | Some r -> r :: 
-                if nullable_p grm p
-                then derive_rule grm x (Rule (s, ps))
-                else []
-        in
-        begin
-          Hashtbl.replace derive_rule_cache (x, rule) res;
-          res
-        end
-
-let remove_redundant_rules old_rules new_rules =
-  let all_rules = List.rev_append new_rules old_rules in
-  let get_rules name =
-    List.find_all (function 
-      | Rule (name', _) -> name = name') all_rules in
-  let rec loop = function
-    | [] -> []
-    | (Rule(name, ps)) as r :: rs ->
-        if List.exists (function
-          | NT name -> get_rules name = []
-          | _ -> false 
-        ) ps
-        then loop rs
-        else r :: loop rs
-  in
-    fix eq_lists loop new_rules
-
-let derive_grm x = function 
-	| Grm(start, rules) as grm -> 
-        Grm(make_nt x start,
-            rules @@
-            (remove_redundant_rules rules (
-            List.concat (List.rev_map (derive_rule grm x) rules))))
-
-let recognize seq grm =
-	let rec loop (Grm (start, rules) as grm) = function
-		| [] -> 
-        begin
-          (*print_endline "DONE";*)
-          (*print_endline (pp_grm grm);*)
-          (*print_endline ("Nullables: " ^ String.concat " " (nullable_symbs grm));*)
-          List.exists (fun s -> s = start) (nullable_symbs grm)
-        end
-		| c :: seq -> 
-        begin
-          print_endline ("["^string_of_int (List.length rules)^"] @'"^String.make 1 c^"'");
-          (*print_endline "Deriving...";*)
-          let grm' = derive_grm c grm in
-          begin
-            (*print_endline "finished derive_grm";*)
-            loop grm' seq
-          end
-        end
-	in
-		loop grm seq
-		
-let rec_str str grm = recognize (explode str) grm
-
-let p_str = "1*1+1+1/1*1+1"
-
-let _ = begin
-	print_endline ("Start parsing: " ^ p_str);
-	let ps = rec_str p_str expr_grm in
-	if ps then print_endline "OK"
-	else print_endline "Not member"
-end
+let exp_grm =
+  let open Grammar in
+  add_rule "Exp" (Rule[
+    [NT "Num"];
+    [NT "Exp"; Lit '+'; NT "Exp"]
+  ]) (
+  add_rule "Num" (Rule[
+    [NT "Digit"];
+    [NT "Num"; NT "Digit"]
+  ]) (
+  add_rule "Digit" (Rule[
+    [Lit '0'];
+    [Lit '1'];
+    [Lit '2'];
+    [Lit '3'];
+    [Lit '4'];
+    [Lit '5'];
+    [Lit '6'];
+    [Lit '7'];
+    [Lit '8'];
+    [Lit '9'];
+  ]) (make_grm "Exp")))
